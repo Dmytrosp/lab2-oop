f4600a1dcb8c5d1483ff89676fde6d546e805e98
Dmytro Polishchuk <dmytrosp@gmail.com>
2020-11-25 20:11:58 +0200

    init lab2

---
+++ XmlParser/XmlParser/Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace XmlParser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.comboBox6 = new System.Windows.Forms.ComboBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.button1Filter = new System.Windows.Forms.Button();
+            this.button2Clear = new System.Windows.Forms.Button();
+            this.button3HTML = new System.Windows.Forms.Button();
+            this.button4Open = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(287, 10);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(293, 391);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(16, 99);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(46, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Title";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(91, 99);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(182, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(91, 136);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(182, 21);
+            this.comboBox2.TabIndex = 5;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(16, 136);
+            this.checkBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(49, 17);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "Artist";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(91, 172);
+            this.comboBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(182, 21);
+            this.comboBox3.TabIndex = 7;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(16, 175);
+            this.checkBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(62, 17);
+            this.checkBox3.TabIndex = 6;
+            this.checkBox3.Text = "Country";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(91, 211);
+            this.comboBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(182, 21);
+            this.comboBox4.TabIndex = 9;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(16, 211);
+            this.checkBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(70, 17);
+            this.checkBox4.TabIndex = 8;
+            this.checkBox4.Text = "Company";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Location = new System.Drawing.Point(91, 248);
+            this.comboBox5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(182, 21);
+            this.comboBox5.TabIndex = 11;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(16, 248);
+            this.checkBox5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(50, 17);
+            this.checkBox5.TabIndex = 10;
+            this.checkBox5.Text = "Price";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // comboBox6
+            // 
+            this.comboBox6.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox6.FormattingEnabled = true;
+            this.comboBox6.Location = new System.Drawing.Point(91, 284);
+            this.comboBox6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.comboBox6.Name = "comboBox6";
+            this.comboBox6.Size = new System.Drawing.Size(182, 21);
+            this.comboBox6.TabIndex = 13;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(16, 284);
+            this.checkBox6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(48, 17);
+            this.checkBox6.TabIndex = 12;
+            this.checkBox6.Text = "Year";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(16, 24);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(70, 17);
+            this.radioButton1.TabIndex = 14;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "DOM API";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(98, 24);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(66, 17);
+            this.radioButton2.TabIndex = 15;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "SAX API";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(178, 24);
+            this.radioButton3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(87, 17);
+            this.radioButton3.TabIndex = 16;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "LINQ to XML";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // button1Filter
+            // 
+            this.button1Filter.Location = new System.Drawing.Point(16, 321);
+            this.button1Filter.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1Filter.Name = "button1Filter";
+            this.button1Filter.Size = new System.Drawing.Size(94, 29);
+            this.button1Filter.TabIndex = 17;
+            this.button1Filter.Text = "Filter";
+            this.button1Filter.UseVisualStyleBackColor = true;
+            this.button1Filter.Click += new System.EventHandler(this.button1Filter_Click_1);
+            // 
+            // button2Clear
+            // 
+            this.button2Clear.Location = new System.Drawing.Point(178, 321);
+            this.button2Clear.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2Clear.Name = "button2Clear";
+            this.button2Clear.Size = new System.Drawing.Size(94, 29);
+            this.button2Clear.TabIndex = 18;
+            this.button2Clear.Text = "Clear";
+            this.button2Clear.UseVisualStyleBackColor = true;
+            this.button2Clear.Click += new System.EventHandler(this.button2Clear_Click);
+            // 
+            // button3HTML
+            // 
+            this.button3HTML.Location = new System.Drawing.Point(16, 362);
+            this.button3HTML.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button3HTML.Name = "button3HTML";
+            this.button3HTML.Size = new System.Drawing.Size(256, 26);
+            this.button3HTML.TabIndex = 19;
+            this.button3HTML.Text = "XML to HTML";
+            this.button3HTML.UseVisualStyleBackColor = true;
+            this.button3HTML.Click += new System.EventHandler(this.button3HTML_Click);
+            // 
+            // button4Open
+            // 
+            this.button4Open.Location = new System.Drawing.Point(16, 53);
+            this.button4Open.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button4Open.Name = "button4Open";
+            this.button4Open.Size = new System.Drawing.Size(256, 29);
+            this.button4Open.TabIndex = 20;
+            this.button4Open.Text = "Open XML file";
+            this.button4Open.UseVisualStyleBackColor = true;
+            this.button4Open.Click += new System.EventHandler(this.button4Open_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 410);
+            this.Controls.Add(this.button4Open);
+            this.Controls.Add(this.button3HTML);
+            this.Controls.Add(this.button2Clear);
+            this.Controls.Add(this.button1Filter);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.comboBox6);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "XmlParser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.ComboBox comboBox6;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Button button1Filter;
+        private System.Windows.Forms.Button button2Clear;
+        private System.Windows.Forms.Button button3HTML;
+        private System.Windows.Forms.Button button4Open;
+    }
+}
+
---
+++ XmlParser/XmlParser/SaxXmlParser.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using XmlParser.Models;
+
+namespace XmlParser
+{
+    class SaxXmlParser : IXmlParserStrategy
+    {
+        public Catalog Parse(string xmlFile, CD cd)
+        {
+            var xmlReader = new XmlTextReader(xmlFile);
+            xmlReader.WhitespaceHandling = WhitespaceHandling.None;
+
+            List<CD> cds = new List<CD>();
+            int elementsCount = 6;
+            string title = "";
+            string artist = "";
+            string country = "";
+            string company = "";
+            string price = "";
+            string year = "";
+
+            while (xmlReader.Read())
+            {
+                string name;
+                string value;
+                int i = 0;
+
+                switch (xmlReader.NodeType)
+                {
+                    case XmlNodeType.Element:
+                        if (xmlReader.Name.ToLower() == "cd")
+                        {
+                            while (i < elementsCount)
+                            {
+                                xmlReader.Read();
+                                name = xmlReader.Name.ToLower();
+
+                                xmlReader.Read();
+                                value = xmlReader.Value;
+                                xmlReader.Read();
+
+                                if (name == "title" && (cd.Title == "" || cd.Title == value))
+                                    title = value;
+                                else if (name == "artist" && (cd.Artist == "" || cd.Artist == value))
+                                    artist = value;
+                                else if (name == "country" && (cd.Country == "" || cd.Country == value))
+                                    country = value;
+                                else if (name == "company" && (cd.Company == "" || cd.Company == value))
+                                    company = value;
+                                else if (name == "price" && (cd.Price == "" || cd.Price == value))
+                                    price = value;
+                                else if (name == "year" && (cd.Year == "" || cd.Year == value))
+                                    year = value;
+
+                                i++;
+                            }
+                        }
+                        break;
+                    case XmlNodeType.EndElement:
+                        if(xmlReader.Name.ToLower() == "cd")
+                        {
+                            if (title != "" && artist != "" && country != ""
+                                && company != "" && price != "" && year != "")
+                            {
+                                CD cd1 = new CD(title, artist, country, company, price, year);
+                                cds.Add(cd1);
+                            }
+
+                            title = "";
+                            artist = "";
+                            country = "";
+                            company = "";
+                            price = "";
+                            year = "";
+                        }
+                        break;
+                }
+            }
+            xmlReader.Close();
+
+            Catalog catalog = new Catalog() { CDs = cds };
+            return catalog;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
